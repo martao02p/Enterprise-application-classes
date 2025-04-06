4d212dfc1d1582ec9e835dd509c2ebc461a0fc83
marta <55180118+martao02p@users.noreply.github.com>
2025-04-06 16:29:22 +0200

    Add files via upload

---
+++ Bookings/bookings/src/main/java/lab/bookings/controllers/ApartmentController.java
@@ -0,0 +1,46 @@
+package lab.bookings.controllers;
+
+import lab.bookings.models.Apartment;
+import lab.bookings.repositories.ApartmentRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import jakarta.validation.Valid;
+import org.springframework.validation.Errors;
+
+@Controller
+@RequestMapping("/apartments")
+public class ApartmentController {
+
+    private ApartmentRepository repository;
+
+    public ApartmentController(ApartmentRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping()
+    public String getAll(Model model) {
+        model.addAttribute(new Apartment());
+        model.addAttribute("apartments", repository.findAll());
+        return "apartments";
+    }
+
+//    @PostMapping()
+//    public String create(Apartment apartment) {
+//        repository.save(apartment);
+//        return "redirect:/apartments";
+//    }
+    @PostMapping()
+    public String createTable(@Valid Apartment apartment, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("apartments", repository.findAll());
+            return "apartments";
+        } else {
+            repository.save(apartment);
+            return "redirect:/apartments";
+        }
+    }
+
+}
