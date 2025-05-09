f6b8a8ae7fd4901f3ec1718477d39d0c9ea929fe
marta <55180118+martao02p@users.noreply.github.com>
2025-05-09 09:25:25 +0200

    Add files via upload

---
+++ WebApiAjax/WebApiAjax/CalcResult.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiAjax
+{
+    public class CalcResult
+    {
+        public double Sum { get; set; }
+        public double Difference { get; set; }
+        public double Product { get; set; }
+        public double Quotient { get; set; }
+    }
+}
---
+++ WebApiAjax/WebApiAjax/Program.cs
@@ -0,0 +1,47 @@
+using WebApiAjax;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+app.MapGet("/math", (double x, double y) =>
+{
+    return new CalcResult
+    {
+        Sum = x + y,
+        Difference = x - y,
+        Product = x * y,
+        Quotient = y != 0 ? x / y : 0
+    };
+});
+
+app.UseStaticFiles();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
